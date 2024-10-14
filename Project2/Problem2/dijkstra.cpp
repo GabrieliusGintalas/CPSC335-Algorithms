5d6e446571870cca76ac35b11eaa0907a4b2e04d
Gabrielius Gintalas <gabrieliusgintalas@csu.fullerton.edu>
2024-10-14 14:28:01 -0700

    Added testcases for problem 2

---
+++ Project2/Problem2/dijkstra.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+#include "dijkstra.h"
+
+using namespace std;
+
+int GetMinDistance(vector<vector<int>>& paths, int n, int k){
+    
+}
---
+++ Project2/Problem2/dijkstra.h
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int GetMinDistance(vector<vector<int>>& paths, int n, int k);
